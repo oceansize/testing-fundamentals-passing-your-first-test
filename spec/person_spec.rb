553519bf400c4466ecc330f0215c1ccb7a0c50ea
roidriscoll <roi.driscoll@gmail.com>
2015-04-02 23:42:22 +0100

    Adds test for full_name

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,14 @@
 require_relative "../lib/person"
 
 describe Person do
+
   describe "#full_name" do
+
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new(first_name: "Geezer", last_name: "Butler")
+
+      expect(person.full_name).to eq("Geezer Butler")
     end
+
   end
 end
